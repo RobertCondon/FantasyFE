bc3c8b4342ce44396fb2410af8997f61b0c58636
RobertCondon <robert.condon@soeidental.com>
2024-03-07 00:44:12 +1300

    Make players area look a lot better.

--- src/routes/players/+page.svelte
+++ src/routes/players/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { onMount } from 'svelte';
     import { fetchGet } from '$lib/apiService.js';
-    import { Select, Label, Table, TableBody, TableHead, TableHeadCell, Search } from 'flowbite-svelte';
+    import { Card, Select, Label, Table, TableBody, TableHead, TableHeadCell, Search } from 'flowbite-svelte';
     import { writable } from 'svelte/store';
     import PlayerRow from './PlayerRow.svelte';
     let selected;
@@ -54,6 +54,13 @@
         }
     };
 
+    const getSortArrow = (key) => {
+        if ($sortKey === key) {
+            return $sortDirection === 1 ? '▲' : '▼';
+        }
+        return '';
+    };
+
     $: {
         let filteredPlayers = players.filter((item) => item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)
         if (selectedCurrentPosition !== 'any') {
@@ -89,66 +96,72 @@
     }
 </script>
 
+<div style="display: flex">
+    <Card class="mr-3 h-[572px]">
+        <div style="margin-bottom: 2rem;" >
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Filters</h5>
+            <div style="display: flex; align-items: flex-end; margin-top: 2rem">
+                <label style="width: 20rem"> Name
+                    <Search placeholder="Search by name" hoverable={true} bind:value={searchTerm}></Search>
+                </label>
+            </div>
 
-<div style="margin-bottom: 2rem;display: flex;align-content: center;justify-content: space-between;" >
-    <div style="display: flex; align-items: flex-end">
-        <label style="width: 20rem"> Name
-            <Search placeholder="Search by name" hoverable={true} bind:value={searchTerm}></Search>
-        </label>
-    </div>
+            <div style="display: flex; align-items: flex-end; margin-top: 2rem">
+                <label style="width: 20rem"> Max Price
+                    <Search placeholder="Maximum price" hoverable={true} bind:value={maxPrice}></Search>
+                </label>
+            </div>
 
-    <div style="display: flex; align-items: flex-end">
-        <label style="width: 20rem"> Max Price
-            <Search placeholder="Maximum price" hoverable={true} bind:value={maxPrice}></Search>
-        </label>
-    </div>
+            <div style="width: 20rem; margin-top: 2rem">
+                <Label>
+                    Teams
+                    <Select placeholder="Select team" class="mt-2" bind:value={selected} on:change={() => findPlayers()}>
+                        <option selected value="all">All</option>
 
-    <div style="width: 20rem">
-        <Label>
-            Teams
-            <Select placeholder="Select team" class="mt-2" bind:value={selected} on:change={() => findPlayers()}>
-                <option selected value="all">All</option>
+                        {#each teamSelection as { value, name }}
+                            <option {value}>{name}</option>
+                        {/each}
+                    </Select>
+                </Label>
+            </div>
 
-                {#each teamSelection as { value, name }}
-                    <option {value}>{name}</option>
+            <div style="margin-top: 2rem">
+                <Label>
+                    Positions
+                    <Select placeholder="Select current postion" class="mt-2" bind:value={selectedCurrentPosition}>
+                        <option selected value="any">Any</option>
+
+                        {#each currentPositions as { value, name }}
+                            <option {value}>{name}</option>
+                        {/each}
+                    </Select>
+                </Label>
+            </div>
+        </div>
+    </Card>
+    <div class="table-container">
+        <Table shadow striped={true} hoverable={true} divClass="my-table">
+            <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                Players
+                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">All current pickable fantasy players. Click to see each round they have played..</p>
+            </caption>
+            <TableHead class="bg-[#FFFFFF]">
+                <TableHeadCell></TableHeadCell>
+                <TableHeadCell on:click={() => sortTable('name')}>Name</TableHeadCell>
+                <TableHeadCell on:click={() => sortTable('cost')}>Price</TableHeadCell>
+                <TableHeadCell on:click={() => sortTable('owned_by')}>Owned By</TableHeadCell>
+                <TableHeadCell on:click={() => sortTable('position')}>Position</TableHeadCell>
+                <TableHeadCell on:click={() => sortTable('current_position')}>Current Position</TableHeadCell>
+                <TableHeadCell on:click={() => sortTable('fantasy_points_average')}>Avg Fantasy</TableHeadCell>
+            </TableHead>
+            <TableBody class="divide-y">
+                {#each $sortPlayers as player}
+                    <PlayerRow {player} />
                 {/each}
-            </Select>
-        </Label>
-    </div>
-
-    <div>
-        <Label>
-            Positions
-            <Select placeholder="Select current postion" class="mt-2" bind:value={selectedCurrentPosition}>
-                <option selected value="any">Any</option>
-
-                {#each currentPositions as { value, name }}
-                    <option {value}>{name}</option>
-                {/each}
-            </Select>
-        </Label>
+            </TableBody>
+        </Table>
     </div>
 </div>
 
-<Table shadow striped={true} hoverable={true}>
-    <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-        Players
-        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">All current pickable fantasy players. Click to see each round they have played..</p>
-    </caption>
-    <TableHead>
-        <TableHeadCell></TableHeadCell>
-        <TableHeadCell on:click={() => sortTable('name')}>Name</TableHeadCell>
-        <TableHeadCell on:click={() => sortTable('cost')}>Price</TableHeadCell>
-        <TableHeadCell on:click={() => sortTable('owned_by')}>Owned By</TableHeadCell>
-        <TableHeadCell on:click={() => sortTable('position')}>Position</TableHeadCell>
-        <TableHeadCell on:click={() => sortTable('current_position')}>Current Position</TableHeadCell>
-        <TableHeadCell on:click={() => sortTable('fantasy_points_average')}>Avg Fantasy</TableHeadCell>
-    </TableHead>
-    <TableBody class="divide-y">
-        {#each $sortPlayers as player}
-            <PlayerRow {player} />
-        {/each}
-    </TableBody>
-</Table>
 <style>
 </style>
